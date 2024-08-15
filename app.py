fe6d5cdf1a8169fd18f55068de01d6ca612dc22d
sagrev666 <ssagrev@hotmail.com>
2024-08-14 20:28:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,174 @@
 import streamlit as st 
+import numpy as np 
 import pandas as pd
+import plotly.offline as py 
+#py.init_notebook_mode(connected=True) # this code, allow us to work with offline plotly version
+import plotly.graph_objs as go # it's like "plt" of matplot
+import plotly.tools as tls # It's useful to we get some tools of plotly
+
+#definir funciones
+def get_eda(dataset):
+    # Distribución de Creditos por Tipo de Casa
+    trace0 = go.Bar(
+        x=dataset[dataset["risk"] == 'good']["housing"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'good']["housing"].value_counts().values,
+        name='Good credit'
+    )
+
+    trace1 = go.Bar(
+        x=dataset[dataset["risk"] == 'bad']["housing"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'bad']["housing"].value_counts().values,
+        name="Bad Credit"
+    )
+
+    data = [trace0, trace1]
+
+    layout = go.Layout(
+        title='Housing Distribution'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    st.plotly_chart(fig)
+
+    # Distribución de Creditos por Genero
+    trace0 = go.Bar(
+        x=dataset[dataset["risk"] == 'good']["sex"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'good']["sex"].value_counts().values,
+        name='Good credit'
+    )
+
+    trace1 = go.Bar(
+        x=dataset[dataset["risk"] == 'bad']["sex"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'bad']["sex"].value_counts().values,
+        name="Bad Credit"
+    )
+
+    data = [trace0, trace1]
+
+    layout = go.Layout(
+        title='Gender Distribution'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    st.plotly_chart(fig)
+
+    # Distribución de Creditos por Job
+    trace0 = go.Bar(
+        x=dataset[dataset["risk"] == 'good']["job"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'good']["job"].value_counts().values,
+        name='Good credit'
+    )
+
+    trace1 = go.Bar(
+        x=dataset[dataset["risk"] == 'bad']["job"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'bad']["job"].value_counts().values,
+        name="Bad Credit"
+    )
+
+    data = [trace0, trace1]
+
+    layout = go.Layout(
+        title='Job Distribution'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    st.plotly_chart(fig)
+
+    # Distribución de Creditos por Cuentas de ahorro
+    trace0 = go.Bar(
+        x=dataset[dataset["risk"] == 'good']["saving_accounts"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'good']["saving_accounts"].value_counts().values,
+        name='Good credit'
+    )
+
+    trace1 = go.Bar(
+        x=dataset[dataset["risk"] == 'bad']["saving_accounts"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'bad']["saving_accounts"].value_counts().values,
+        name="Bad Credit"
+    )
+
+    data = [trace0, trace1]
+
+    layout = go.Layout(
+        title='Saving Accounts Distribution'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    st.plotly_chart(fig)
+
+
+    # Distribución de Creditos por Cuentas de Crédiro
+    trace0 = go.Bar(
+        x=dataset[dataset["risk"] == 'good']["checking account"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'good']["checking account"].value_counts().values,
+        name='Good credit'
+    )
+
+    trace1 = go.Bar(
+        x=dataset[dataset["risk"] == 'bad']["checking account"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'bad']["checking account"].value_counts().values,
+        name="Bad Credit"
+    )
+
+    data = [trace0, trace1]
+
+    layout = go.Layout(
+        title='Checking Account Distribution'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    st.plotly_chart(fig)
+
+    # Distribución de Creditos por duración
+    trace0 = go.Bar(
+        x=dataset[dataset["risk"] == 'good']["duration"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'good']["duration"].value_counts().values,
+        name='Good credit'
+    )
+
+    trace1 = go.Bar(
+        x=dataset[dataset["risk"] == 'bad']["duration"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'bad']["duration"].value_counts().values,
+        name="Bad Credit"
+    )
+
+    data = [trace0, trace1]
+
+    layout = go.Layout(
+        title='Duration Distribution'
+    )
+
+    fig = go.Figure(data=data, layout=layout)   
+    st.plotly_chart(fig)
+    
+    # Distribución de Creditos por Propósito
+    trace0 = go.Bar(
+        x=dataset[dataset["risk"] == 'good']["purpose"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'good']["purpose"].value_counts().values,
+        name='Good credit'
+    )
+
+    trace1 = go.Bar(
+        x=dataset[dataset["risk"] == 'bad']["purpose"].value_counts().index.values,
+        y=dataset[dataset["risk"] == 'bad']["purpose"].value_counts().values,
+        name="Bad Credit"
+    )
+
+    data = [trace0, trace1]
+
+    layout = go.Layout(
+        title='Purpose Distribution'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    st.plotly_chart(fig)
+
 
 #writing simple text 
 
@@ -32,6 +201,8 @@ if "Explorar Datos" in selected_page:
     st.write("""
     ## Explore Data
     Distributions""")
+    if uploaded_file is not None:
+        get_eda(dataset)
         
 if "Feature Engineering" in selected_page:
     st.write("""
